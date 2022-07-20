d3eb2e563481de513ebe27219f6976692fb8e92c
LongLee220 <78303456+LongLee220@users.noreply.github.com>
2022-07-20 09:34:14 +0800

    Update BFS.py

--- BFS.py
+++ BFS.py
@@ -60,26 +60,14 @@ class Graph:
 
 # Create a graph given in
 # the above diagram
-# g = Graph()
-# g.addEdge(0, 1)
-# g.addEdge(0, 2)
-# g.addEdge(1, 2)
-# g.addEdge(2, 0)
-# g.addEdge(2, 3)
-# g.addEdge(3, 3)
+g = Graph()
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
 
-# print ("Following is Breadth First Traversal"
-# 				" (starting from vertex 2)")
-# g.BFS(2)
-
-# This code is contributed by Neelam Yadav
-
-f = open("0.edges", "r")
-G = Graph()
-lines = f.readlines()
-for line in lines:
-    tmp = line.strip().split(' ')
-    G.addEdge(int(tmp[0]), int(tmp[1]))
 print ("Following is Breadth First Traversal"
-				" (starting from vertex 1)")
-G.BFS(1)
+ 				" (starting from vertex 2)")
+g.BFS(2)
